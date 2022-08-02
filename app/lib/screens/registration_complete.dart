d1f57cfcc420b838fc8b9f95746fef2d5049967e
Florian Niederhuber <f.niederhuber@gmail.com>
2022-08-02 03:22:25 +0200

    not beautiful but does it

--- app/lib/screens/registration_complete.dart
+++ app/lib/screens/registration_complete.dart
@@ -11,48 +11,57 @@ class RegistrationComplete extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<http.Response>(
-        future: this.registerRequest,
-        builder: (BuildContext context, AsyncSnapshot<http.Response> snapshot) {
-          var user = jsonDecode("${snapshot.data?.body}");
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("Registraiton Confirmation"),
+        ),
+        body: FutureBuilder<http.Response>(
+            future: this.registerRequest,
+            builder:
+                (BuildContext context, AsyncSnapshot<http.Response> snapshot) {
+              if (snapshot.hasData) {
+                final UserDto user =
+                    UserDto.fromJson(jsonDecode("${snapshot.data?.body}"));
 
-          if (snapshot.hasData) {
-            return Column(children: [
-              const Icon(
-                Icons.check,
-                color: Colors.green,
-                size: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Hello ${user.first_name} ${user.last_name}'),
-              ),
-            ]);
-          } else if (snapshot.hasError) {
-            return Column(children: [
-              const Icon(
-                Icons.error_outline,
-                color: Colors.red,
-                size: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Register user failed. Error: ${snapshot.error}'),
-              )
-            ]);
-          } else {
-            return Column(children: const [
-              SizedBox(
-                width: 60,
-                height: 60,
-                child: CircularProgressIndicator(),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text('Registering user ...'),
-              )
-            ]);
-          }
-        });
+                return Column(children: [
+                  const Icon(
+                    Icons.check,
+                    color: Colors.green,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text('Hello ${user.first_name} ${user.last_name}'),
+                  ),
+                ]);
+              } else if (snapshot.hasError) {
+                return Column(children: [
+                  const Icon(
+                    Icons.error_outline,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child:
+                        Text('Register user failed. Error: ${snapshot.error}'),
+                  )
+                ]);
+              } else {
+                return Column(children: const [
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: CircularProgressIndicator(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Text('Registering user ...'),
+                  )
+                ]);
+              }
+            }));
   }
 }
