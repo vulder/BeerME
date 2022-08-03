5f52f0a4be4337e3dcb8c02e59c3e485180eac87
Florian Niederhuber <f.niederhuber@gmail.com>
2022-08-03 12:28:53 +0200

    fixed rendering issue

--- app/lib/screens/statistics.dart
+++ app/lib/screens/statistics.dart
@@ -17,57 +17,42 @@ class StatisticsFragment extends StatefulWidget {
 class _StatisticsFragmentState extends State<StatisticsFragment> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(
-      child: Consumer<UserModel>(builder: (context, user, child) {
-        if (!user.hasValidId()) {
-          return const Text("Please register your device fist.");
-        }
+    return Consumer<UserModel>(builder: (context, user, child) {
+      if (!user.hasValidId()) {
+        return const Text("Please register your device fist.");
+      }
 
-        return FutureBuilder<Response>(
-            future: Api.retrieveStatistics(user.id),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                if (snapshot.data?.statusCode == 200) {
-                  var statistics = StatisticsDto.fromJson(
-                      jsonDecode("${snapshot.data?.body}"));
+      return FutureBuilder<Response>(
+          future: Api.retrieveStatistics(user.id),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data?.statusCode == 200) {
+                var statistics = StatisticsDto.fromJson(
+                    jsonDecode("${snapshot.data?.body}"));
 
-                  return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'Today: ${statistics.today}',
-                        ),
-                        Text(
-                          'Week: ${statistics.week}',
-                        ),
-                        Text(
-                          'Month: ${statistics.month}',
-                        ),
-                        Text(
-                          'Total ${statistics.total}',
-                        ),
-                        Padding(
-                            padding: const EdgeInsets.only(top: 16),
-                            child: IconButton(
-                                icon: const Icon(Icons.refresh),
-                                tooltip: 'Refresh',
-                                onPressed: () => setState(() {})))
-                      ]);
-                } else {
-                  return Column(children: [
-                    const Icon(
-                      Icons.error_outline,
-                      color: Colors.red,
-                      size: 60,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16),
-                      child: Text(
-                          'Data retrieval failed with status code ${snapshot.data?.statusCode}.'),
-                    )
-                  ]);
-                }
-              } else if (snapshot.hasError) {
+                return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Today: ${statistics.today}',
+                      ),
+                      Text(
+                        'Week: ${statistics.week}',
+                      ),
+                      Text(
+                        'Month: ${statistics.month}',
+                      ),
+                      Text(
+                        'Total ${statistics.total}',
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: IconButton(
+                              icon: const Icon(Icons.refresh),
+                              tooltip: 'Refresh',
+                              onPressed: () => setState(() {})))
+                    ]);
+              } else {
                 return Column(children: [
                   const Icon(
                     Icons.error_outline,
@@ -76,25 +61,38 @@ class _StatisticsFragmentState extends State<StatisticsFragment> {
                   ),
                   Padding(
                     padding: const EdgeInsets.only(top: 16),
-                    child:
-                        Text('Data retrieval failed. Error: ${snapshot.error}'),
-                  )
-                ]);
-              } else {
-                return Column(children: const [
-                  SizedBox(
-                    width: 60,
-                    height: 60,
-                    child: CircularProgressIndicator(),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 16),
-                    child: Text('Retrieving user data ...'),
+                    child: Text(
+                        'Data retrieval failed with status code ${snapshot.data?.statusCode}.'),
                   )
                 ]);
               }
-            });
-      }),
-    ));
+            } else if (snapshot.hasError) {
+              return Column(children: [
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child:
+                      Text('Data retrieval failed. Error: ${snapshot.error}'),
+                )
+              ]);
+            }
+
+            return Column(children: const [
+              SizedBox(
+                width: 60,
+                height: 60,
+                child: CircularProgressIndicator(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text('Retrieving user data ...'),
+              )
+            ]);
+          });
+    });
   }
 }
