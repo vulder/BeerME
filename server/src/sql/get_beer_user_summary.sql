e4c91f19138ec5cdb593bd27b613567da521fd0a
Tristan Schneider <schneidtha@gmail.com>
2022-08-05 01:59:22 +0200

    Summary time interval fix (#9)

--- server/src/sql/get_beer_user_summary.sql
+++ server/src/sql/get_beer_user_summary.sql
@@ -1,5 +1,6 @@
 select * from
  (select count(*) as total from beer_user.beers where uuid = $1) as total,
- (select count(*) as today from beer_user.beers where uuid = $1 and time > current_date - interval '1 day') as today,
- (select count(*) as week  from beer_user.beers where uuid = $1 and time > current_date - interval '1 week') as week,
- (select count(*) as month from beer_user.beers where uuid = $1 and time > current_date - interval '1 month') as month;
+ (select count(*) as today from beer_user.beers where uuid = $1 and time > date_trunc('day', now())) as today,
+ (select count(*) as week  from beer_user.beers where uuid = $1 and time > date_trunc('week', now())) as week,
+ (select count(*) as month from beer_user.beers where uuid = $1 and time > date_trunc('month', now())) as month;
+ 
